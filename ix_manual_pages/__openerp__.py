383fc73356b946010e514ced521abea4ecfdf7fd
Afwan Wali Hamim <afwanwalihamim@gmail.com>
2015-04-17 05:37:19 +0700

    [FIX] fixing init, openerp, and py comment header

---
+++ ix_manual_pages/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Afwan Wali Hamim
+#    Copyright (C) 2015 Afwan Wali Hamim <afwanwalihamim@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Enchancement of Document Page by iXotem',
+    'version': '1.0',
+    'category': 'Knowledge',
+    'sequence': 1,
+    'summary': 'Step by step self help.',
+    'description': """
+        Provide step-by-step help which is integrated inside document page module.
+        You could make tutorial of using odoo or anything else with this module.
+    """,
+    'author': 'Afwan Wali Hamim',
+    'website': 'http://ixotem.blogspot.com/',
+    'depends': ['document_page'],
+    'data': [
+        'ix_doc_step_view.xml',
+    ],
+    'demo': [],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- ix_manual_pages/ix_doc_step.py
+++ ix_manual_pages/ix_doc_step.py
@@ -1,7 +1,28 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Afwan Wali Hamim
+#    Copyright (C) 2015 Afwan Wali Hamim <afwanwalihamim@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 from osv import orm, fields
 
 
-class ix_doc_step(orm.Model)
+class ix_doc_step(orm.Model):
     _name = 'ix.doc.step'
     _columns = {
         'name': fields.char('Name'),
