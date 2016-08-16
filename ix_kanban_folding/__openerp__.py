ee80f0d50384595fc9f3fe63551c405f3d120b46
Afwan Wali Hamim <afwanwalihamim@gmail.com>
2016-08-16 14:39:20 +0700

    [ADD] ix_kanban_folding: New module for fold kanban stage when no record

---
+++ ix_kanban_folding/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Fold Zero Record Kanban",
+    "version": "0.1",
+    "author": "Afwan Wali Hamim",
+    "summary": "Fold your kanban stage when have no record.",
+    "category": "ix Module/General",
+    "description": """
+    This module will make kanban stage will automatically folded if no record when you visit the kanban view at the first time.
+    """,
+    "website": "http://afwan.id/",
+    "depends": [
+        "web_kanban",
+        ],
+    "data": [
+        "views/kanban_view.xml",
+    ],
+    "css": [],
+    "js": [],
+    "installable": True,
+    "active": False,
+    "application": False,
+}
---
+++ ix_kanban_folding/static/src/js/kanban_column.js
@@ -0,0 +1,13 @@
+odoo.define('ix_kanban_folding.ix_kanban_column', function (require) {
+"use strict";
+var KanbanColumn = require('web_kanban.Column');
+
+KanbanColumn.include({
+    start: function() {
+        if (this.data_records.length === 0){
+            this.folded = true;
+        }
+        this._super.apply(this, arguments);
+    },
+});
+});
